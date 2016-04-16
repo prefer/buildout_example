0bb0608dd5fa0bc97ae156cabd8e665768e5cf5a
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2016-04-17 01:07:54 +0300

    Remove python-dev dependency

--- provision.sh
+++ provision.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y python-dev
-
 wget -O /tmp/bootstrap.py https://bootstrap.pypa.io/bootstrap-buildout.py
 cd /vagrant/buildout_example
 python /tmp/bootstrap.py
 bin/buildout
+bin/django-manage migrate
