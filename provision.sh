11db3cab9b0c370673b388401826d74b1418e32f
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2016-04-17 22:16:15 +0300

    Remove README

--- provision.sh
+++ provision.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# Get buildout bootstrap file and run
 wget -O /tmp/bootstrap.py https://bootstrap.pypa.io/bootstrap-buildout.py
 cd /vagrant/buildout_example
 python /tmp/bootstrap.py
 
+# Provision by buildout. For details see buildout.cfg
 bin/buildout -U
+
+# Run migration
 bin/django-manage migrate --run-syncd
