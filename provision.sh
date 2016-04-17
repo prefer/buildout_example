7cd15c8c539430d6aff8620272836135aaa9ea2f
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2016-04-17 16:34:06 +0300

    Support todo mvc bacbone+django

--- provision.sh
+++ provision.sh
@@ -4,4 +4,4 @@ wget -O /tmp/bootstrap.py https://bootstrap.pypa.io/bootstrap-buildout.py
 cd /vagrant/buildout_example
 python /tmp/bootstrap.py
 bin/buildout
-bin/django-manage migrate
+#bin/django-manage migrate
