2364d8970cc02326c75ca6ff67730177db44d635
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2016-04-23 00:06:18 +0300

    Added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -r /vagrant/buildout_example/bin
+rm -r /vagrant/buildout_example/develop-eggs
+rm -r /vagrant/buildout_example/eggs
+rm -r /vagrant/buildout_example/parts
+rm -r /vagrant/buildout_example/src
+rm /vagrant/buildout_example/.installed.cfg
+rm /vagrant/buildout_example/.mr.developer.cfg
